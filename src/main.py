37d072cbf87796090aac355c530182c507d10445
Samuel Brito <samuelbrito@ufop.edu.br>
2025-05-19 22:11:15 -0300

    Aula 19/05

--- src/main.py
+++ src/main.py
@@ -2,4 +2,19 @@ from matrizAdjacencias import MatrizAdjacencias
 from listaAdjacencias import ListaAdjacencias
 
 if __name__ == "__main__":
-    print("Ola, mundo!")
+    grafo = MatrizAdjacencias(5)
+    grafo.addAresta(0, 1, 2)
+    grafo.addAresta(0, 2, 4)
+    grafo.addAresta(1, 4, 7)
+    grafo.addAresta(4, 0, 3)
+
+    print(f"Ordem: {grafo.ordem()}")
+    print(f"Tamanho: {grafo.tamanho()}")
+
+    grafo.printGrafo()
+
+    print(f"Vizinhos de {0}: {grafo.vizinhos(0)}")
+    print(f"Vizinhos de {1}: {grafo.vizinhos(1)}")
+    print(f"Vizinhos de {2}: {grafo.vizinhos(2)}")
+    print(f"Vizinhos de {3}: {grafo.vizinhos(3)}")
+    print(f"Vizinhos de {4}: {grafo.vizinhos(4)}")
--- src/matrizAdjacencias.py
+++ src/matrizAdjacencias.py
@@ -2,34 +2,43 @@
 class MatrizAdjacencias:
     # inicializa o grafo:    
     def __init__(self, numVertices):
-        print("Implementar construtor da classe MatrizAdjacencias\n")
+        self.numVertices = numVertices
+        self.numArestas = 0
+        self.matriz = []
+
+        for i in range(self.numVertices):
+            tmp = []
+            for j in range(self.numVertices):
+                tmp.append(0)
+            self.matriz.append(tmp)
 
     # retorna a ordem do grafo:
     def ordem(self):
-        print("Metodo ordem(self) nao foi implementado ainda!\n")
-        return None
+        return self.numVertices
     
     # retorna o tamanho do grafo:
     def tamanho(self):
-        print("Metodo tamanho(self) nao foi implementado ainda!\n")
-        return None
+        return self.numArestas
 
     # adiciona uma aresta (v1, v2) no grafo:
     # peso eh um parametro opcional
     def addAresta(self, v1, v2, peso = 1):
-        print("Metodo addAresta(self, v1, v2, peso = 1) nao foi implementado ainda!\n")
-        return None
+        if self.matriz[v1][v2] == 0:
+            self.numArestas += 1
+        self.matriz[v1][v2] = peso
 
     # retorna True se existe uma aresta (v1,v2) no grafo:
     def possuiAresta(self, v1, v2):
-        print("Metodo possuiAresta(self, v1, v2) nao foi implementado ainda!\n")
-        return None
+        return self.matriz[v1][v2] != 0
 
     # retorna uma lista de tuplas (vertice, peso)
     # com os vizinhos de v:
     def vizinhos(self, v):
-        print("Metodo vizinhos(self, v) nao foi implementado ainda!\n")
-        return None
+        viz = []
+        for j in range(self.numVertices):
+            if self.matriz[v][j] != 0:
+                viz.append(j)
+        return viz
     
     # retorna o grau (saida) de um vertice:
     def grau(self, v):
@@ -38,5 +47,7 @@ class MatrizAdjacencias:
 
     # exibe o grafo no formato de matriz de adjacencias:
     def printGrafo(self):
-        print("Metodo printGrafo(self) nao foi implementado ainda!\n")
-        return None
+        for i in range(self.numVertices):
+            for j in range(self.numVertices):
+                print(self.matriz[i][j],end=" ")
+            print()
